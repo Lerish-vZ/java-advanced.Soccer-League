1e38c709b68138b43f36ffec0870a8aa52c4954b
Lerish-vZ <lerishavz@gmail.com>
2024-09-03 13:41:14 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    static Scoreboard scoreboard = new Scoreboard();
+    static Match match = new Match();
+
     public static void main(String[] args) {
-        Scoreboard scoreboard = new Scoreboard();
-        Match match = new Match();
         try {
             File file = new File("C:\\Users\\lerishav\\ownCloud - lerishav@cloud.zylex.co.za\\Documents\\All my work\\Code College\\7_Java_Advanced\\Java_Advanced_Projects\\SoccerLeague\\src\\Soccer_Matches.txt");
             Scanner sc = new Scanner(file);
@@ -16,6 +17,7 @@ public class Main {
                 String t1Score = line.substring((line.indexOf(",") - 1), (line.indexOf(",")));
                 String teamName2 = line.substring((line.lastIndexOf(",") + 2), line.lastIndexOf(" "));
                 String t2Score = line.substring(line.lastIndexOf(" ")).trim();
+                calcScore(teamName1, t1Score, teamName2, t2Score);
                 scoreboard.setScoreboard(teamName1);
                 scoreboard.setScoreboard(teamName2);
                 match.setMatch(teamName1, teamName2, t1Score, t2Score);
@@ -27,5 +29,15 @@ public class Main {
             System.out.println("Error occurred.");
             e.printStackTrace();
         }
+    }
+
+    public static void calcScore(String team1, String score1, String team2, String score2) {
+        int iScore1 = Integer.parseInt(score1);
+        int iScore2 = Integer.parseInt(score2);
+
+        if(iScore1 == iScore2){
+            scoreboard.incScore(team1, 1);
+            scoreboard.incScore(team2, 1);
+        } 
     }
 }
--- src/Match.java
+++ src/Match.java
@@ -5,7 +5,8 @@ public class Match {
     private ArrayList<String> match = new ArrayList<>();
 
     public void setMatch(String team1, String team2, String score1, String score2) {
-        this.match.add(("Match: " + team1 + " " + score1 + " | " + team2 + " " + score2));
+        this.match.add((team1 + " " + score1 + " | " + team2 + " " + score2));
+
     }
 
     public ArrayList<String> getMatch() {
