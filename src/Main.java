6593f9c3531c8ef5623e772c3c3642915bc6861a
Lerish-vZ <lerishavz@gmail.com>
2024-09-03 14:43:08 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Path path = Paths.get("C:\\Users\\lerishav\\ownCloud - lerishav@cloud.zylex.co.za\\Documents\\All my work\\Code College\\7_Java_Advanced\\Java_Advanced_Projects\\SoccerLeague\\src\\Soccer_Matches.txt");
+    
+    }
 }
