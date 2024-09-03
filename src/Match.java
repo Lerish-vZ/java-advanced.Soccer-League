1abed9513f64221dff4bf13308a560c3ea404a3e
Lerish-vZ <lerishavz@gmail.com>
2024-09-03 11:48:55 +0200

    update

--- src/Match.java
+++ src/Match.java
@@ -11,5 +11,5 @@ public class Match {
         return match;
     }
 
-    
+    public
 }
--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -1,7 +1,15 @@
 import java.util.HashMap;
 
 public class Scoreboard {
-    HashMap <String, Integer> teams = new HashMap<>(6);
+    HashMap <String, Integer> score = new HashMap<>(6);
+    
+    public Scoreboard(String teamName) {
+        score.put(teamName, 0);
+    }
+
+    public void incScore(String teamName, int inc) {
+
+    }
 
     
 }
