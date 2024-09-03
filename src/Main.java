3c417ad2b3683cb43e355f09063a05be123a0c08
Lerish-vZ <lerishavz@gmail.com>
2024-09-03 11:36:16 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            File file = new File("C:\\Users\\lerishav\\ownCloud - lerishav@cloud.zylex.co.za\\Documents\\All my work\\Code College\\7_Java_Advanced\\Java_Advanced_Projects\\SoccerLeague\\src\\Soccer_Matches.txt");
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                Match match = new Match(line);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error occurred.");
+            e.printStackTrace();
+        }
     }
 }
--- src/Match.java
+++ src/Match.java
@@ -1,5 +1,15 @@
 import java.io.*;
 
 public class Match {
+    private String match;
+
+    public Match(String match) {
+        this.match = match;
+    }
+
+    public String getMatch() {
+        return match;
+    }
+
     
 }
